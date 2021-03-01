055b0581acad5b88200b2f2433c0ca6e7bd9507a
zerodoc <danielc@smallwoodhome.com>
2021-03-01 16:22:26 -0600

    add imgui to project structure

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # git-submodule
+
 Just practicing with submodules
+
+To add submodules use:
+
+`git submodule add https://github.com/<user>/<repo> vendor/<repo>`
+
+To fetch submodules use:
+
+`git submodule update --init --recursive`
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@
 
 #include "spdlog/spdlog.h"
 #include "spdlog/cfg/env.h"
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include "GL/glew.h"
 #include "GLFW/glfw3.h"
 
 namespace slog = spdlog;
@@ -25,41 +30,18 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 }
 
 int main() {
-    std::cout << "start program" << std::endl;
+    slog::set_pattern("[%H:%M:%S] [%t] [%^%l%$] %v");
+    slog::set_level(slog::level::trace);
+    slog::info("Start program...");
 
-    slog::info("Using spdlog version {}.{}.{}!", SPDLOG_VER_MAJOR, SPDLOG_VER_MAJOR, SPDLOG_VER_PATCH);
-
-    slog::warn("Easy padding in numbers like {:08d}", 12);
-    slog::error("Example of an error in spdlog");
-    slog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
-    slog::info("Support for floats {:03.2f}", 1.23456);
-    slog::info("Positional args are {1} {0}...", "too", "supported");
-    slog::info("{:>8} aligned, {:<8} aligned", "right", "left");
-
-    // Runtime log levels
-    slog::set_level(slog::level::info); // Set global log level to info
-    slog::debug("This message should not be displayed!");
-    slog::set_level(slog::level::trace); // Set specific logger's log level
-    slog::debug("This message should be displayed...");
-
-    slog::set_pattern("[%H:%M:%S] [%o] [%^%l%$] [%t] %v");
-    slog::info("the start of sleep");
-
-    // Customize msg format for all loggers
-    
-    slog::info("This an info message with custom format");
-    // slog::set_pattern("%+"); // back to default format
-    slog::set_level(slog::level::info);
-
-    // Backtrace support
-    // Loggers can store in a ring buffer all messages (including debug/trace) for later inspection.
-    // When needed, call dump_backtrace() to see what happened:
-    slog::enable_backtrace(10); // create ring buffer with capacity of 10  messages
-    for (int i = 0; i < 100; i++) {
-        slog::debug("Backtrace message {}", i); // not logged..
-    }
-    // e.g. if some error happened:
-    slog::dump_backtrace(); // log them now!
+    /* 
+        slog::enable_backtrace(10); // create ring buffer with capacity of 10  messages
+        for (int i = 0; i < 100; i++) {
+            slog::debug("Backtrace message {}", i); // not logged..
+        }
+        
+        slog::dump_backtrace(); // e.g. if some error happened call this function
+    */
 
     slog::info("Starting window creation...");
 
@@ -72,7 +54,11 @@ int main() {
         return 1;
     }
 
+    slog::debug("Adding glfw window hints");
     glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
     GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
     if (window == NULL) 
     {
@@ -83,17 +69,61 @@ int main() {
         glfwTerminate();
         return 1;
     }
+    glfwMakeContextCurrent(window);
+
+    bool err = glewInit() != GLEW_OK;
+    if(err) {
+        slog::error("Failed to init OpenGL loader");
+        slog::shutdown();
+        glfwTerminate();
+        return 1;
+    }
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    bool show_demo_window = true;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     glfwSetKeyCallback(window, key_callback);
     while(!glfwWindowShouldClose(window) && running) {
-        glfwSwapBuffers(window);
         glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        if(show_demo_window) ImGui::ShowDemoWindow(&show_demo_window);
+
+
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
     }
-    slog::debug("Destroying window");
+
+    slog::debug("Cleaning up ImGui...");
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    slog::debug("Destroying window...");
     glfwDestroyWindow(window);
     glfwTerminate();
 
-    slog::info("End of program");
+    slog::info("End of program...");
     slog::shutdown();
 
     return 0;
